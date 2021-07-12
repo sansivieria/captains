67f92db168ddd8df07259c9e5d053a05ef142eca
Forest Traylor <foresttraylor@gmail.com>
2021-07-12 04:24:37 -0700

    create route

--- server.js
+++ server.js
@@ -7,22 +7,29 @@ const PORT = 3000;
 // jsx engine
 app.set('view engine', 'jsx');
 app.engine('jsx', require('express-react-views').createEngine());
-
+app.use(express.urlencoded({extended:true}))
 // restful routes
 
 // index
 
 // new
 app.get('/logs/new',(req,res)=>{
-  res.render('New')
-})
+  res.render('New');
+});
 
 // delete
 
 // update
 
 // create
-
+app.post('/logs',(req,res)=>{
+  if(req.body.shipIsBroken ==='on') {
+     req.body.shipIsBroken = true;
+  } else {
+    req.body.shipIsBroken = false;
+  }
+  res.send(req.body);
+});
 // edit
 
 // show
